8251327f75ac8a99dc4b864558c9ad1a58797e19
pkqs90 <pkqs90@gmail.com>
2023-12-05 15:04:40 +0800

    public key

---
+++ contracts/accounts/PublicKeyChallenge.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.21;
+
+contract PublicKeyChallenge {
+    address owner = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    bool public isComplete;
+
+    function toString(address account) public pure returns(string memory) {
+        return toString(abi.encodePacked(account));
+    }
+
+    function toString(uint256 value) public pure returns(string memory) {
+        return toString(abi.encodePacked(value));
+    }
+
+    function toString(bytes32 value) public pure returns(string memory) {
+        return toString(abi.encodePacked(value));
+    }
+
+    function toString(bytes memory data) public pure returns(string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(2 + data.length * 2);
+        str[0] = "0";
+        str[1] = "x";
+        for (uint i = 0; i < data.length; i++) {
+            str[2+i*2] = alphabet[uint(uint8(data[i] >> 4))];
+            str[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
+        }
+        return string(str);
+    }
+
+    function authenticate(bytes publicKey) public {
+        string memory err = toString(publicKey);
+        require(address(keccak256(publicKey)) == owner, err);
+
+        isComplete = true;
+    }
+}
---
+++ test/accounts/public-key.js
@@ -0,0 +1,52 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { fixtureFactory } = require("../utils");
+const assert = require('node:assert').strict;
+
+function calculatePublicKey(tx) {
+  // Since Hardhat uses eip1559 by default, we should pass in `maxPriorityFeePerGas` and `maxFeePerGas` instead of legacy `gasPrice`.
+  const txData = {
+    gasLimit: tx.gasLimit,
+    value: tx.value,
+    nonce: tx.nonce,
+    data: tx.data,
+    to: tx.to,
+    chainId: tx.chainId,
+    maxPriorityFeePerGas: tx.maxPriorityFeePerGas,
+    maxFeePerGas: tx.maxFeePerGas,
+  };
+  const newTx = ethers.Transaction.from(txData);
+  const newTxSerialized = newTx.unsignedSerialized;
+  const newTxHash = ethers.keccak256(newTxSerialized);
+  const pk = ethers.SigningKey.recoverPublicKey(newTxHash, tx.signature);
+  return pk;
+}
+
+it("Solves GuessTheNewNumberChallenge", async function () {
+  const [signer] = await ethers.getSigners();
+  const fixture = () => fixtureFactory("PublicKeyChallenge");
+  const contract = await loadFixture(fixture);
+  const address = signer.getAddress();
+
+  // Dummy transaction.
+  const txResponse = await signer.sendTransaction({
+      to: address
+  });
+
+  // This is the private key of signer, which deployed challenge `PublicKeyChallenge`. We can use 
+  // the deployment tx to calculate its public key.
+  // Public key:            0x048318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed753547f11ca8696646f2f3acb08e31016afac23e630c5d11f59f61fef57b0d2aa5
+  // Compressed public key: 0x038318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed75
+  // Address =              0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+  const signingKey = new ethers.SigningKey("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
+  const publicKey = signingKey.publicKey;
+
+  // Calculate pk by txResponse data.
+  const pk = calculatePublicKey(txResponse);
+
+  assert(publicKey === pk);
+  // Since the public keys are uncompressed, they contain a `04` prefix which should be thrown away.
+  const rawPk = `0x${publicKey.slice(4)}`;
+  await contract.authenticate(rawPk);
+  expect(await contract.isComplete()).to.equal(true);
+});
